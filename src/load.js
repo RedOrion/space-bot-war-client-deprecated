b9e903c470b796133309c77ce40924f27dcdf9f7
Iain C Docherty <github@iandocherty.com>
2013-12-01 14:42:45 -0500

    Sorta got it working (at least it outputs the HTML correctly)

--- src/load.js
+++ src/load.js
@@ -1,19 +1,23 @@
 require.config({
     paths: {
         jquery          : 'js/jquery-2.0.3',
+        'jquery.bootstrap'       : 'js/bootstrap',
         jqueryUI        : 'js/jquery-ui-1.10.3.custom',
         underscore      : 'js/underscore',
         templates       : '../templates',
     },
     shim: {
         jqueryUI        : ['jquery'],
+        "jquery.bootstrap" : {
+            deps: ["jquery"]
+        },
         underscore      : {
             exports     : '_',
         },
     }
 });
 
-requirejs(['jquery', 'game'], function($, Game) {
+requirejs(['jquery', 'game', 'jquery.bootstrap', 'game'], function($, Game) {
 
     if (typeof(window.serverUrl) == 'undefined') {
         window.url = window.location.protocol + '//' + window.server + '.spacebotwar.com';
