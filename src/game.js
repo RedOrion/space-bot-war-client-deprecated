3b846458b77f4b72fdb336be2c786d4c0bbafeb3
Ian Docherty <git@iandocherty.com>
2013-12-02 08:15:09 +0000

    Can now connect to local Web Socket server.

--- src/game.js
+++ src/game.js
@@ -9,6 +9,26 @@ function($, Template, TmplGame) {
             $('#sbw').html(Template.read.game_main_screen({
                 foo     : 'bar'
             }));
+
+            // Make a connection to the lobby
+
+            var ws = new WebSocket("ws://localhost:5000/ws/game/lobby");
+
+            ws.onerror = function(e) {
+                alert('cannot connect '+e);
+            };
+
+            ws.onopen = function() {
+                alert('connected');
+            };
+
+            ws.onmessage = function(e) {
+                var data = $.evalJSON(e.data);
+                var type = data.type;
+                var content = data.content;
+
+                alert(e.data);
+            };
         };
     }
     
